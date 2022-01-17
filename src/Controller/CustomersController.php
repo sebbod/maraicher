ec5cf452d2ea073a2ab5102fc6585b53fb37367a
sebbod <sebbod@laposte.net>
2022-01-17 14:16:37 +0100

    php bin/console make:crud Customers

---
+++ src/Controller/CustomersController.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Customers;
+use App\Form\CustomersType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/customers")
+ */
+class CustomersController extends AbstractController
+{
+    /**
+     * @Route("/", name="customers_index", methods={"GET"})
+     */
+    public function index(EntityManagerInterface $entityManager): Response
+    {
+        $customers = $entityManager
+            ->getRepository(Customers::class)
+            ->findAll();
+
+        return $this->render('customers/index.html.twig', [
+            'customers' => $customers,
+        ]);
+    }
+
+    /**
+     * @Route("/new", name="customers_new", methods={"GET", "POST"})
+     */
+    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $customer = new Customers();
+        $form = $this->createForm(CustomersType::class, $customer);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($customer);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('customers_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('customers/new.html.twig', [
+            'customer' => $customer,
+            'form' => $form,
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="customers_show", methods={"GET"})
+     */
+    public function show(Customers $customer): Response
+    {
+        return $this->render('customers/show.html.twig', [
+            'customer' => $customer,
+        ]);
+    }
+
+    /**
+     * @Route("/{id}/edit", name="customers_edit", methods={"GET", "POST"})
+     */
+    public function edit(Request $request, Customers $customer, EntityManagerInterface $entityManager): Response
+    {
+        $form = $this->createForm(CustomersType::class, $customer);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->flush();
+
+            return $this->redirectToRoute('customers_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('customers/edit.html.twig', [
+            'customer' => $customer,
+            'form' => $form,
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="customers_delete", methods={"POST"})
+     */
+    public function delete(Request $request, Customers $customer, EntityManagerInterface $entityManager): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$customer->getId(), $request->request->get('_token'))) {
+            $entityManager->remove($customer);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('customers_index', [], Response::HTTP_SEE_OTHER);
+    }
+}
---
+++ src/Form/CustomersType.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Customers;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class CustomersType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name')
+            ->add('phone')
+            ->add('mobile')
+            ->add('email')
+            ->add('siret')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Customers::class,
+        ]);
+    }
+}
